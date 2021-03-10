9caf4b1a72046b68c60613e56638f0f915170a94
bakuchi- <helveticaf0015se@gmail.com>
2021-03-10 11:41:34 +0900

    fix logger.py mkdir

--- src/logger.py
+++ src/logger.py
@@ -1,7 +1,12 @@
 from logging import INFO, getLogger, StreamHandler, Formatter, FileHandler, DEBUG
+import os
 
 
-def setup_logger(log_folder, modname=__name__):
+def setup_logger(file_name, modname=__name__):
+    dir = os.path.dirname(filepath)
+    if not os.path.isdir(dir):
+        os.makedirs(dir)
+
     logger = getLogger(modname)
     logger.setLevel(DEBUG)
 
@@ -12,7 +17,7 @@ def setup_logger(log_folder, modname=__name__):
     sh.setFormatter(formatter)
     logger.addHandler(sh)
 
-    fh = FileHandler(log_folder)
+    fh = FileHandler(file_name)
     fh.setLevel(INFO)
     fh_formatter = Formatter(
         '%(asctime)s:%(levelname)s:%(filename)s:%(lineno)d:%(message)s')
@@ -22,7 +27,8 @@ def setup_logger(log_folder, modname=__name__):
 
 
 if __name__ == '__main__':
-    logger = setup_logger('log/logger_test.log')
+    filepath = 'log/test/mkdir_test.log'
+    logger = setup_logger(filepath)
     logger.debug('Log test')
     listed_markets = [{'name': 'BTC-PERP', 'ask': 100}]
     logger.info(listed_markets)
