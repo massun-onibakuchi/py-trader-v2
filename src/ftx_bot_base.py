86beb40995dc7a8f3ad619acd41998f9ae12241f
bakuchi- <helveticaf0015se@gmail.com>
2021-04-03 13:34:02 +0900

    fix

--- src/ftx_bot_base.py
+++ src/ftx_bot_base.py
@@ -1,8 +1,8 @@
 from typing import Dict, Union, List, Any
 import asyncio
-import inspect
 import time
 from ftx.ftx import FTX
+from bot_base_error import APIRequestError, PositionCycleError, OrderCycleError, _message
 from line import push_message
 from setting.settting import PYTHON_ENV, config
 from logger import setup_logger
@@ -12,71 +12,6 @@ MARKET = config['MARKET']
 TRADABLE = config.getboolean('TRADABLE')
 VERBOSE = config.getboolean('VERBOSE')
 PUSH_NOTIF = config.getboolean('PUSH_NOTIF')
-
-
-def _message(data='', msg_type=''):
-    text = ''
-    if isinstance(data, str):
-        text = data
-    elif isinstance(data, Dict):
-        # dataがpositionデータの時
-        if 'netSize' in data and 'side' in data:
-            base = f'Position netSize:{data["netSize"]} side:{data["side"]} '
-            if msg_type.lower() == 'update':
-                text = 'Update' + base
-            elif msg_type.lower() == 'sync':
-                text = 'Sync' + base
-        # dataがオーダーデータの時
-        if 'orderId' in data and 'status' in data:
-            base = f'order:{data["orderId"]} status:{data["status"]} '
-            if msg_type.lower() == 'new':
-                text = 'New' + base
-            elif msg_type.lower() == 'update':
-                text = 'Update' + base
-            elif msg_type.lower() == 'cancel':
-                text = 'Cancel' + base
-            elif 'side' in data and 'price' in data and 'type' in data:
-                text = base + f'price:{data["price"]} type:{data["type"]} side:{data["side"]}'
-            else:
-                text = base
-    if text == '':
-        text = '_unexpexted_data_type_'
-    return text
-
-
-class CycleError(Exception):
-    def __init__(self, expression, msg_type=''):
-        self.expression = expression
-        self.msg_type = msg_type
-
-    def __str__(self):
-        if self.error_name == '':
-            self.error_name = 'CycleError'
-        return self._error_message()
-
-    def _error_message(self):
-        return f'{self.error_name}:{inspect.stack()[1].function} {_message(self.expression,self.msg_type)}'
-
-
-class OrderCycleError(CycleError):
-    def __init__(self, order, msg_type):
-        super().__init__(order, msg_type)
-        self.error_name = 'OrderCycleError'
-
-
-class PositionCycleError(CycleError):
-    def __init__(self, data, msg_type):
-        super().__init__(data, msg_type)
-        self.error_name = 'PositionCycleError'
-
-
-class APIRequestError(Exception):
-    def __init__(self, expression, msg=""):
-        self.expression = expression
-        self.msg = msg
-
-    def __str__(self):
-        return f'APIRequestError:{inspect.stack()[1].function} {self.expression}:{self.msg}'
 
 
 class BotBase:
