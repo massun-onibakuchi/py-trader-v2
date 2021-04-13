0aa179956108c7f620459e2215e732ad9bb322ee
bakuchi- <helveticaf0015se@gmail.com>
2021-04-13 18:48:54 +0900

    fixed tweet_long

--- src/tweet_long.py
+++ src/tweet_long.py
@@ -1,133 +1,83 @@
+"""
+    BotBaseを継承したボット．
+    エントリーのみで決済しない．
+"""
 import asyncio
-from datetime import datetime, timezone, timedelta
-from ftx.ftx import FTX
-from twitter.recent_research import recent_research
-from line import push_message
-from setting.settting import FTX_API_KEY, FTX_API_SECRET, SUBACCOUNT, PYTHON_ENV, config
-from pprint import pprint
+from ftx_bot_base import BotBase
+from setting.settting import PYTHON_ENV, FTX_API_KEY, FTX_API_SECRET, SUBACCOUNT, config
+from twitter.recent_research import recent_research, create_time_fields
 
-BOT_NAME = config["BOT_NAME"]
-MARKET = config["MARKET"]
-MAX_SIZE = float(config["MAX_SIZE"])
-TRADABLE = config.getboolean('TRADABLE')
+MARKET = config['MARKET']
+MARKET_TYPE = config["MARKET_TYPE"]
+
+MAX_POSITION_SIZE = config.getfloat('MAX_POSITION_SIZE')
+SEC_TO_EXPIRE = config.getfloat('SEC_TO_EXPIRE')
+SIZE = config.getfloat('SIZE')
 
 
-class Bot:
-    MAX_POSITION_SIZE: float = 0.0
-    # ---------------------------------------- #
-    # init
-    # ---------------------------------------- #
-
-    def __init__(self, api_key, api_secret):
-        self.order_list = []
-        if MAX_SIZE is not None:
-            self.MAX_POSITION_SIZE = float(MAX_SIZE)
-        else:
-            raise ValueError("MAX_SIZE is neither int nor float")
-        self.ftx = FTX(
-            MARKET,
-            api_key=api_key,
-            api_secret=api_secret,
-            subaccount=SUBACCOUNT)
-
-        print(
-            "BOT_NAME: %s \nENV:%s \nMARKET %s \nSUBACCOUNT: %s"
-            % (BOT_NAME,
-                PYTHON_ENV,
-                MARKET,
-                SUBACCOUNT))
+class Bot(BotBase):
+    def __init__(self, market, market_type, api_key, api_secret, subaccount):
+        super().__init__(market, market_type, api_key, api_secret, subaccount)
+        self.MARKET = MARKET
+        self.SIZE = SIZE
         # タスクの設定およびイベントループの開始
         loop = asyncio.get_event_loop()
-        tasks = [self.run()]
-
+        # tasks = [self.run_strategy()]
+        tasks = [self.run(10), self.run_strategy()]
         loop.run_until_complete(asyncio.wait(tasks))
 
-    # ---------------------------------------- #
-    # bot main
-    # ---------------------------------------- #
-    async def run(self):
+    async def run_strategy(self):
         while True:
             try:
-                await self.main(5)
-                await asyncio.sleep(0)
+                await self.strategy(10)
+                await asyncio.sleep(10)
             except Exception as e:
-                print('An exception occurred', e)
-                push_message(e)
-                exit(1)
+                self.logger.error(f'Unhandled Error :strategy {str(e)}')
+                self.push_message(f'Unhandled Error :strategy {str(e)}')
 
-    def create_time_fields(self, sec=10):
-        since_date = ""
-        td = ""
-        utc_date = datetime.now(timezone.utc)
-        if PYTHON_ENV != 'production':  # テストの時
-            td = timedelta(days=3)
-            since_date = utc_date - td
-        else:  # 本番のとき 検索開始期間を10s前に設定
-            td = timedelta(seconds=sec)
-            since_date = utc_date - td
-        start_time_fields = "start_time=" + \
-            since_date.strftime("%Y-%m-%dT%H:%M:%SZ")
-        return start_time_fields
-
-    async def main(self, interval):
-        # main処理
-
-        self.ftx.positions()
-        response = await self.ftx.send()
-        # pprint(response[0])
-        position = {}
-        for pos in response[0]["result"]:
-            if pos["future"] == MARKET:
-                position = pos
-        print("\nPOSITION :>>")
-        pprint(position)
-
-        await asyncio.sleep(5)
-
-        # 現在のポジションが規定のサイズを超えていたらエントリーしない
-        if position["netSize"] > float(self.MAX_POSITION_SIZE):
-            print("\n[Info]: MAX_POSITION_SIZE")
+    async def strategy(self, interval):
+        # pos, success = await self.get_position()
+        # if not success:
+        #     return await asyncio.sleep(5)
+        # elif pos["size"] > self.MAX_POSITION_SIZE:
+        #     msg = f'MAX_POSITION_SIZE current size:{pos["size"]}'
+        #     self.logger.info(msg)
+        #     self.push_message(msg)
+        if 1:
+            pass
         else:
             query = "query=from:elonmusk -is:retweet"
             tweet_fields = "tweet.fields=author_id"
-            start_time_fields = self.create_time_fields(sec=12)
+            start_time_fields = create_time_fields(sec=12)
             queries = [query, tweet_fields, start_time_fields]
             keywords = ['doge', 'Doge', 'DOGE']
 
             result = recent_research(keywords, queries)
 
             if len(result) > 0:
-                push_message(f"Detect events:\nkeywords:{keywords}\n{result}")
-                if TRADABLE:
-                    if PYTHON_ENV == 'production':
-                        self.ftx.place_order(
-                            ord_type='market',
-                            market=MARKET,
-                            side='buy',
-                            price='',
-                            size=3800,
-                            postOnly=False)
-                    else:
-                        self.ftx.place_order(
-                            ord_type='limit',
-                            market=MARKET,
-                            side='buy',
-                            price=1111,
-                            size=0.001,
-                            postOnly=True)
-                    response = await self.ftx.send()
-                    pprint(response[0])
-                    orderId = response[0]['result']['id']
-                    self.order_list.append({
-                        "timestamp": datetime.now(timezone.utc),
-                        "orderId": orderId
-                    })
-                    push_message(
-                        f"[Order]{PYTHON_ENV}\nMARKET:{MARKET}\norderId:{orderId}"
+                self.push_message(f"Detect events:\nkeywords:{keywords}\n{result}")
+                if PYTHON_ENV == 'production':
+                    await self.place_order(
+                        ord_type='market',
+                        side='buy',
+                        price='',
+                        size=self.SIZE,
+                        postOnly=False,
+                        ioc=True,
+                        sec_to_expire=SEC_TO_EXPIRE
                     )
+                else:
+                    await self.place_order(
+                        ord_type='limit',
+                        side='buy',
+                        price=1111,
+                        size=0.001,
+                        postOnly=True,
+                        sec_to_expire=SEC_TO_EXPIRE
+                    )
+
         await asyncio.sleep(interval)
 
 
 if __name__ == "__main__":
-
-    Bot(api_key=FTX_API_KEY, api_secret=FTX_API_SECRET)
+    bot = Bot(MARKET, MARKET_TYPE, FTX_API_KEY, FTX_API_SECRET, SUBACCOUNT)
